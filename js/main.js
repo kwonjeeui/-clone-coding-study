3d2c27b1b9661d51a53584cf9340eee54d1c8f00
권제의 <kwonjeeui@gmail.com>
2024-06-03 17:55:21 +0900

    main 페이지 작업중

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-var swiper = new Swiper(".swiper", {
-  slidesPerView: 1,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
+$(document).ready(function(){
+  var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
 });
